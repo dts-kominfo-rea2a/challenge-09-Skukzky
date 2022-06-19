e7364c87e5a7379300c8dc255e4be5ed17a92e42
Skukzky <rifkioskar1@gmail.com>
2022-06-19 15:51:38 +0700

    Tantangan: Pengenalan React

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,20 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
+import './Contact.css'
 
-const Contact = () => {
+const Contact = ({ data }) => {
     return (
-        <></>
+        <div className='contact'>
+            <div>
+                <img className='contact-photo' src={data.photo} alt={data.name} />
+            </div>
+            <div>
+                <p className='contact-name'>{data.name}</p>
+                <p className='contact-phone'>{data.phone}</p>
+                <p className='contact-email'>{data.email}</p>
+            </div>
+        </div>
     )
 }
 
